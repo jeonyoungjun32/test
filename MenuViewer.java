1d4d64caab885aac2953c1b95e56667df86eb990
jeonyoungjun32 <ddydwns123@naver.com>
2021-07-28 12:09:47 +0900

    Revert "7월28일 삭제합니다"
    This reverts commit a7818b6d97e8484189ea94dc5ac7e6151bb326c4.

---
+++ MenuViewer.java
@@ -0,0 +1,18 @@
+package a_project_interface_test;
+
+import java.util.Scanner;
+
+public class MenuViewer {
+	//멤버변수
+	//public static Scanner sc = new Scanner(System.in);
+	
+	//메서드
+	public static void showMenu() {
+		System.out.println("선택하세요...");
+		System.out.println("1. 고객 데이터 입력");
+		System.out.println("2. 주민번호로 이름과 전화번호 검색");
+		System.out.println("3. 프로그램 종료");
+		System.out.print("선택: ");
+		
+	}
+}
